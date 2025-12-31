9153e68297e24eef4360cfc2f8387fbe4e6f761a
jingyipotato <thexchanted@gmail.com>
2026-01-01 01:54:00 +0800

    feat: add logging function

---
+++ utils/logging.py
@@ -0,0 +1,26 @@
+"""Script for logging."""
+
+import logging
+import sys
+
+def get_logger(name: str) -> logging.Logger:
+    """Return a formatted logger.
+
+    Args:
+        name: Name of the logger.
+
+    Returns:
+        Configured logger instance.
+
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        console_handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    logger.propagate = False
+    return logger
